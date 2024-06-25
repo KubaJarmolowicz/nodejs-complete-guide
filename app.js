f397857f476465d099f2a6b20d1f205497509ba1
Kuba Jarmołowicz <k.jarmolowicz@bsgroup.eu>
2024-06-25 10:58:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First BE App</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
