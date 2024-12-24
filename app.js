dc35c2833be5acdce86f66f36250cee2fb4edd72
KubaJarmolowicz <kuba.jarmolowicz92@gmail.com>
2024-12-24 13:31:17 +0100

    added user model and a dummy user fetching middleware

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const shopRoutes = require("./routes/shop");
 
 const { get404 } = require("./controllers/error");
 const sequelize = require("./utils/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,14 +19,44 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findByPk(1);
+    req.user = user;
+  } catch (e) {
+    console.log("Cannot assign user to request!");
+  } finally {
+    next();
+  }
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(get404);
 
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE",
+});
+User.hasMany(Product);
+
 sequelize
   .sync()
-  .then((result) => {
+  .then(() => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({
+        name: "Testomir",
+        email: "email@example.com",
+      });
+    }
+
+    return Promise.resolve(user);
+  })
+  .then(() => {
     app.listen(3000, () => {
       console.log("Server is listening on port 3000");
     });
